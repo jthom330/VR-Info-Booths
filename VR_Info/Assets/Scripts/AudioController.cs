ce9bc36ffaed46ff109dbf54fb79f74bd42a8f7f
Wiley <jthom330@gmail.com>
2018-06-25 15:56:56 -0400

    Initial commit of content
    Write-up, images, video, and apk build still need to be added.

---
+++ VR_Info/Assets/Scripts/AudioController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour {
+
+    public AudioClip clip;
+    public GvrAudioSource source;
+
+    void Start()
+    {
+        source.clip = clip;
+    }
+
+    public void PlayAudio()
+    {
+
+        source.Stop();
+        source.Play();
+    }
+}
---
+++ VR_Info/Assets/Scripts/MoveToPoint.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MoveToPoint : MonoBehaviour {
+
+    public TextMeshProUGUI displayText;
+    public GameObject player;
+    public GvrAudioSource[] sources;
+    private Behaviour halo;
+
+
+    void Start()
+    {
+        halo = (Behaviour)gameObject.GetComponent("Halo");
+        halo.enabled = false;
+    }
+
+    public void Move()
+    {
+        //stop any display audio from playing
+        for(int i = 0; i < sources.Length; i++)
+        {
+            sources[i].Stop();
+        }
+        
+        iTween.MoveTo(player,
+            iTween.Hash(
+                "x", gameObject.transform.position.x,
+                "z", gameObject.transform.position.z,
+                "time", .2,
+                "easetype", "linear"
+            )
+        );
+    }
+
+    public void LookAt(string newText)
+    {
+        displayText.text = newText;
+        halo.enabled = true;
+    }
+
+    public void LookAway()
+    {
+        halo.enabled = false;
+        displayText.text = "";
+    }
+
+
+}
